d416dd3754826abb808eff47a4add7eae0687f17
rikiachmad <rikiachmad@yahoo.com>
2021-12-11 22:17:47 +0700

    Add Main Menu and Scores text

--- main.js
+++ main.js
@@ -53,12 +53,51 @@ function init(){
     })
     window.addEventListener('click', selectObject);
 
+    const blocker = document.getElementById( 'blocker' );
+    const instructions = document.getElementById( 'instructions' );
+    const gameover = document.getElementById( 'gameover' );
+    const mainmenu = document.getElementById( 'mainmenu' );
+    const easy = document.getElementById( 'easy' );
+    const medium = document.getElementById( 'medium' );
+    const hard = document.getElementById( 'hard' );
+    const scores = document.getElementById( 'scores' );
     // Add mouse controls
     const controls = new THREE.PointerLockControls( camera, renderer.domElement );
+    var pause = true;
+    var inGame = false;
+   scores.style.display = 'none';
+    blocker.style.display = 'block';
+    easy.addEventListener('click', function (){
+        inGame = true;
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+        mainmenu.style.display = 'none';
+        scores.style.display = '';
+        difficultyEasy();
+    });
+    medium.addEventListener('click', function (){
+        inGame = true;
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+        mainmenu.style.display = 'none';
+        scores.style.display = '';
+        difficultyMedium();
+    });
+    hard.addEventListener('click', function (){
+        inGame = true;
+        instructions.style.display = 'none';
+        blocker.style.display = 'none';
+        mainmenu.style.display = 'none';
+        scores.style.display = '';
+        difficultyHard();
+    });
     document.body.addEventListener( 'click', function () {
-
+        if(!inGame) return;
         controls.lock();
+    } );
+    controls.addEventListener( 'lock', function () {
 
+        pause=false;
     } );
     scene.add( controls.getObject() );
 
@@ -72,12 +111,14 @@ function init(){
     var done = false;
     var canSelect = false;
 
+    let points = [];
     let mesh = [];
     let mesh2 = [];
     let newPoints;
     let balls = [];
     let visited = [];
     var start=null, end=null;
+    var addZ=0;
 
     var width;
     var speed;
@@ -85,16 +126,17 @@ function init(){
     var lineIndex = 0;
     var counter=0;
     var lineColor = [new THREE.Color( 0xff0000 ),new THREE.Color( 0x00FF00 ) ,new THREE.Color( 0x0000FF ) ];
-    // difficultyEasy();
-    difficultyMedium();
-    // difficultyHard();
+
     const tick = () =>
     {
-        if(done){
-            disposeLine();
+        if(!pause){
+            if(done){
+                disposeLine();
+            }
+            animateLine();
+            hover();
         }
-        animateLine();
-        hover();
+
         renderer.render(scene, camera)
 
         window.requestAnimationFrame(tick)
@@ -157,8 +199,9 @@ function init(){
 
         let newX = x1;
         let newY = y1;
+        let newZ = z1;
         for(let j=0; j<100; j++){
-            newPoints.push([newX, newY, z1]);
+            newPoints.push([newX, newY, newZ]);
             newX+=changeX;
             newY+=changeY;
         }
@@ -173,12 +216,12 @@ function init(){
         mesh[index] = new THREE.Mesh(line, material);
         scene.add(mesh[index++]);
     }
+
     function disposeLine(){
         setTimeout(()=>{
             for(let i=0; i<mesh.length; i++){
                 scene.remove(mesh[i]);
             }
-            mesh = [];
             index = 0;
             canSelect = true;
         }, 3000)
@@ -191,9 +234,7 @@ function init(){
         let offset = mesh[lineIndex].material.uniforms.dashOffset.value;
         mesh[lineIndex].material.uniforms.dashOffset.value -= speed;
         if(offset < -1){
-            // if(counter>2)counter=0;
             lineIndex++;
-            // if(lineIndex < mesh.length)mesh[lineIndex].material.uniforms.color.value = lineColor[counter++];
         }
     }
     function createCrosshair(camera) {
@@ -242,11 +283,30 @@ function init(){
         if(start){
 
             end = intersects[0].object.position;
-            drawLine(start.x,end.x, start.y, end.y, start.z, 0.5, 0, mesh2);
+            drawLine(start.x,end.x, start.y, end.y, start.z+addZ, 0.5, 0, mesh2);
             start = end;
+            addZ+=0.03;
+            if(visited.length === mesh.length + 1){
+                if(checkResult())gameContinue();
+                else gameOver();
+            }
             return;
         }
         start = intersects[0].object.position;
+    }
+    function checkResult(){
+        let correct = points;
+        let users = visited;
+        for(let i=0; i<mesh.length; i++){
+             if(correct[i][0]!=users[i].position.x || correct[i][1]!=users[i].position.y )return false;
+        }
+        return true;
+    }
+    function gameOver(){
+
+    }
+    function gameContinue(){
+
     }
     function checkBall(object){
         for(let i=0; i<visited.length; i++){
@@ -255,9 +315,9 @@ function init(){
         return false;
     }
     function difficultyEasy(){
-        speed = 0.03;
+        speed = 0.01;
         width = 1;
-        let points = [];
+        let count = 0;
         let r = 2;
         let x = 10, y=27, z=-20;
         for(let j=0; j<3; j++){
@@ -265,21 +325,21 @@ function init(){
                 points.push([x, y, z]);
                 drawBall(r, x, y, z);
                 x-=10;
-                z+=0.05;
             }
             y-=8;
             x=10;
         }
-       shuffle(points);
+       points = shuffle(points);
         for(let i=0; i<8; i++){
-            drawLine(points[i][0],points[i+1][0], points[i][1], points[i+1][1],points[i][2], 0.7, 10, mesh);
+            drawLine(points[i][0],points[i+1][0], points[i][1], points[i+1][1],points[i][2] + count, 0.7, 10, mesh);
+            count+=0.05;
         }
         counter = 0;
     }
     function difficultyMedium(){
         speed = 0.015;
         width = 0.5;
-        let points = [];
+        let count = 0;
         let r = 1.5;
         let x = 12.5, y=27, z=-20;
         for(let j=0; j<4; j++){
@@ -287,21 +347,21 @@ function init(){
                 points.push([x, y, z]);
                 drawBall(r, x, y, z);
                 x-=8;
-                z+=0.1;
             }
             y-=6;
             x=12.5;
         }
         shuffle(points);
         for(let i=0; i<15; i++){
-            drawLine(points[i][0],points[i+1][0], points[i][1], points[i+1][1],points[i][2], 0.7, 10, mesh);
+            drawLine(points[i][0],points[i+1][0], points[i][1], points[i+1][1],points[i][2]+ count, 0.7, 10, mesh);
+            count+=0.03;
         }
         counter = 0;
     }
     function difficultyHard(){
         speed = 0.01;
         width = 0.3;
-        let points = [];
+        let count = 0;
         let r = 1;
         let x = 13, y=27, z=-20;
         for(let j=0; j<5; j++){
@@ -309,14 +369,14 @@ function init(){
                 points.push([x, y, z]);
                 drawBall(r, x, y, z);
                 x-=6;
-                z+=0.03;
             }
             y-=4.5;
             x=13;
         }
         shuffle(points);
         for(let i=0; i<24; i++){
-            drawLine(points[i][0],points[i+1][0], points[i][1], points[i+1][1],points[i][2], 0.7, 10, mesh);
+            drawLine(points[i][0],points[i+1][0], points[i][1], points[i+1][1],points[i][2]+ count, 0.7, 10, mesh);
+            count+=0.03;
         }
         counter = 0;
     }
